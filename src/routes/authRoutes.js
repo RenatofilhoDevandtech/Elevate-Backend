33cd30a7efa61e8058e76d1ea9b56ebdb5de4867
RenatoFilho <https://github.com/RenatofilhoDevandtech>
2025-06-19 21:20:35 -0300

    feat: :world_map: rotas atualizadas para authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,15 +1,19 @@
-const express = require('express');
+// src/routes/authRoutes.js
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/authController');
-const { protect } = require('../middleware/authMiddleware');
+const authController = require("../controllers/authController");
+const { protect } = require("../middlewares/authMiddleware"); // Caminho padronizado
 
 // Rota pública para registrar um novo usuário
-router.post('/register', authController.register);
+router.post("/register", authController.register);
 
 // Rota pública para fazer login
-router.post('/login', authController.login);
+router.post("/login", authController.login);
 
 // Rota protegida para obter os dados do usuário logado através do token
-router.get('/me', protect, authController.getMe);
+router.get("/me", protect, authController.getMe);
+
+// Rota protegida para fazer logout
+router.post("/logout", protect, authController.logout);
 
 module.exports = router;
